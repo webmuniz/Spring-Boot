71726553b881ff1d68690110a69bc5ee87a9c136
webmuniz <mnz.pessoal@gmail.com>
2022-02-25 15:55:43 -0300

    :wrench: Spring annotations

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -1,18 +1,29 @@
 package academy.devdojo.springboot2.controller;
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.util.DateUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("anime")
+@Log4j2
+@RequiredArgsConstructor
+@Configuration
 
 public class AnimeController {
+    private final DateUtil dateUtil;
+
     @GetMapping(path = "list")
     public List<Anime> list(){
+        log.info(dateUtil.formatLocalDateTimeToDataBaseStyle(LocalDateTime.now()));
         return List.of(new Anime("DBZ"), new Anime("Berserk"));
     }
 }
