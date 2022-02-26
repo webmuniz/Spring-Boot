f6987ecf625c7a372b415e4bade606a8975d6512
webmuniz <mnz.pessoal@gmail.com>
2022-02-26 12:37:54 -0300

    :white_check_mark: Post Method

--- src/main/java/academy/devdojo/springboot2/controller/GameController.java
+++ src/main/java/academy/devdojo/springboot2/controller/GameController.java
@@ -8,10 +8,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -27,14 +24,20 @@ public class GameController {
     private final GameService gameService;
 
     @GetMapping
-    public ResponseEntity<List<Game>> list(){
+    public ResponseEntity<List<Game>> list() {
         log.info(dateUtil.formatLocalDateTimeToDataBaseStyle(LocalDateTime.now()));
         //return new ResponseEntity<>(gameService.listAll(), HttpStatus.OK);
         return ResponseEntity.ok(gameService.listAll());
     }
 
     @GetMapping(path = "/{id}")
-    public ResponseEntity<Game> findById(@PathVariable long id){
+    public ResponseEntity<Game> findById(@PathVariable long id) {
         return ResponseEntity.ok(gameService.findById(id));
+    }
+
+    @PostMapping
+    //@ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Game> save(@RequestBody Game game) {
+        return new ResponseEntity<>(gameService.save(game), HttpStatus.CREATED);
     }
 }
