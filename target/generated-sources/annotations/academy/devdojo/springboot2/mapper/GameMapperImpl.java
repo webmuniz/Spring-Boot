3c5f38fd72dadfaf04d6f3584cc3aaca3b0e4c83
webmuniz <mnz.pessoal@gmail.com>
2022-03-03 22:47:32 -0300

    :white_check_mark: Spring Security - CSRF Token

--- target/generated-sources/annotations/academy/devdojo/springboot2/mapper/GameMapperImpl.java
+++ target/generated-sources/annotations/academy/devdojo/springboot2/mapper/GameMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-03-03T21:45:32-0300",
+    date = "2022-03-03T22:04:23-0300",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.1 (Oracle Corporation)"
 )
 @Component
